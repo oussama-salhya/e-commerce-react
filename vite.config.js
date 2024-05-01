c424c143c0268eacda703f2913b35efc97e20dca
oussama-salhya <oussamasalhya@gmail.com>
2024-05-01 12:08:02 +0100

    first commit, here we go again

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/v1/": "http://localhost:8080",
+    },
+  },
+});
